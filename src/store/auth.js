c9265317ac0e1ebb2383996e34006b3e241070c0
osacrMesh <okerekeifeanyi65@gmail.com>
2023-03-18 15:55:43 +0100

    refactoring

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,64 +1,3 @@
-// export default {
-//   namespaced: true,
-//   state() {
-//     return {
-//       isLoggedIn: false,
-//       user: {
-//         username: "",
-//         email: "",
-//         password: "",
-//       },
-//     };
-//   },
-//   mutations: {
-//     register(state, user) {
-//       state.user = user;
-//       localStorage.setItem("user", JSON.stringify(state.user));
-//     },
-//     login(state) {
-//       state.isLoggedIn = true;
-//       localStorage.setItem("isLoggedIn", true);
-//       location.href = "/products";
-//       localStorage.getItem("user", JSON.stringify(state.user));
-//     },
-//     intializeState(state) {
-//       if (
-//         JSON.parse(localStorage.getItem("isLoggedIn")) &&
-//         localStorage.getItem("user")
-//       ) {
-//         state.isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn"));
-//         state.user = localStorage.getItem("user");
-//       }
-//     },
-//     logout(state) {
-//       state.isLoggedIn = false;
-//       localStorage.setItem("isLoggedIn", false);
-//       localStorage.setItem("user", JSON.stringify(state.user));
-//       console.log(state.user);
-//       // location.href = "/login";
-//     },
-//     clearUser(state) {
-//       state.isLoggedIn = false;
-//       localStorage.setItem("isLoggedIn", false);
-//       state.user = {
-//         username: "",
-//         email: "",
-//         password: "",
-//       };
-//       localStorage.setItem("user", state.user);
-//     },
-//   },
-//   actions: {
-//     login({ commit }, status) {
-//       commit("login", status);
-//     },
-//   },
-//   getters: {
-//     isLoggedIn(state) {
-//       return state.isLoggedIn;
-//     },
-//   },
-// };
 import { useToast } from "vue-toastification";
 import router from "../router";
 const toast = useToast();
@@ -84,15 +23,12 @@ export default {
   },
   actions: {
     async signup({ commit }, user) {
-      // simulate async signup request
       await new Promise((resolve) => setTimeout(resolve, 1000));
       commit("setUser", user);
-      // commit("setIsAuthenticated", true);
       toast.success("You have successfully registered");
       router.push("/login");
     },
     async login({ state, commit }, { email, password }) {
-      // simulate async login request
       await new Promise((resolve) => setTimeout(resolve, 1000));
       const user = state.user;
       if (user && user.email === email && user.password === password) {
@@ -100,7 +36,6 @@ export default {
         toast.success("Login Successful");
         router.push("/products");
       } else {
-        // alert("Invalid email or password");
         toast.error("Invalid email or password");
       }
     },
